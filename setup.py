c490f47867794ab08081fcc3267b472a96a8966f
aaraya <adrian.araya@ridgerun.com>
2025-09-04 16:01:56 -0600

    Remove hailo tappas deps libs build from meson, now the pkg config is used

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# Copyright (C) 2024 RidgeRun, LLC (http://www.ridgerun.com)
+# Copyright (C) 2025 RidgeRun, LLC (http://www.ridgerun.com)
 # All Rights Reserved.
 
 # The contents of this software are proprietary and confidential to RidgeRun,
