0aa9024df1fd0190d1bb26f3815c4c730249f8b3
aaraya <adrian.araya@ridgerun.com>
2025-09-05 14:46:56 -0600

    Add servername=0.0.0.0 to make the server visible locally

--- lpd_app/gui/app.py
+++ lpd_app/gui/app.py
@@ -128,6 +128,7 @@ class App:
 
             demo.queue()
             demo.launch(
+                server_name="0.0.0.0",
                 share=True,
                 show_api=False,
                 favicon_path="lpd_app/gui/assets/icon.png",
